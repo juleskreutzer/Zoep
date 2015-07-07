b7795f86c6e71e91c8b34b20c4194ea9284913bd
juleskreutzer <j.kreutzer@student.fontys.nl>
2015-07-07 21:49:53 +0200

    initial commit

---
+++ Zoep/Zoep/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Zoep
+//
+//  Created by Jules Kreutzer on 07-07-15.
+//  Copyright (c) 2015 Jules Kreutzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var nameArray = [""];
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showInfor(sender: AnyObject) {
+        var alert = UIAlertController(title: "Zeer uitgebreide handleiding", message: "Ohh.. I need to be replaced!", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Begrepen", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+
+}
+
---
+++ Zoep/Zoep/secondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  secondViewController.swift
+//  Zoep
+//
+//  Created by Jules Kreutzer on 07-07-15.
+//  Copyright (c) 2015 Jules Kreutzer. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController : ViewController {
+
+}
